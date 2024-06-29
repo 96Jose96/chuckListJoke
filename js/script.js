b153ba68356030c16dce963eb4e2ce965527aa8c
Jose <jm.diaz6620@gmail.com>
2024-06-29 16:06:38 +0200

    terminado

--- js/script.js
+++ js/script.js
@@ -1,60 +1,73 @@
-const jokeBtn = document.getElementById('fetchJoke'); //boton
-const jokeList = document.getElementById('jokeList'); //list
+const jokeBtn = document.getElementById('fetchJoke'); // Botón obtener chiste
+const jokeList = document.getElementById('jokeList'); // Lista de chistes
 
-
-//obtiene json de la API y lo pinta en pantalla
+// Obtener chiste de la API y mostrar en pantalla
 const getJokeApi = () => {
     jokeBtn.addEventListener('click', () => {
         fetch('https://api.chucknorris.io/jokes/random')
             .then((response) => response.json())
             .then((data) => {
                 const joke = data.value;
-                console.log(joke) //comprobar que trae los datos de la API
-                const template = 
-                `
-                <li>
-                    <h2>${joke}</h2>
-                    <button class = 'removeBtn'>Eliminar</button>
-                </li>
-                `
-                jokeList.innerHTML += template;
+                console.log(joke);
+                addNewJoke(joke);
                 jokesSave(joke);
             })
-    })
+            
+    });
 }
 
-const jokesSave = (joke) => {           //grabar chiste en localStorage
+// Guardar chiste en localStorage
+const jokesSave = (joke) => {
     let jokesLocalStorage = loadJokesStorage();
     jokesLocalStorage.push(joke);
-    localStorage.setItem('newJoke', JSON.stringify(jokesLocalStorage));
+    localStorage.setItem('jokes', JSON.stringify(jokesLocalStorage));
 }
 
-
-const loadJokesStorage = () => {        //cargar chistes de localStorage
-    const jokesLocalStorage = localStorage.getItem('newJoke');
-     return jokesLocalStorage ? JSON.parse(jokesLocalStorage) : [];   
-}
-
-const loadJokesPage = () =>{            //pintar los chistes de localStorage en pagina       
-    jokesLocalStorage = loadJokesStorage();
+// Cargar chistes desde localStorage y mostrar en la página
+const loadJokesPage = () => {
+    const jokesLocalStorage = loadJokesStorage();
     jokesLocalStorage.forEach(joke => {
-        const template = 
-                `
-                <li>
-                    <h2>${joke}</h2>
-                    <button class = 'removeBtn'>Eliminar</button>
-                </li>
-                `
-                jokeList.innerHTML += template;
-    })
+        addNewJoke(joke);
+    });
 }
 
-const removeJoke = () => {          //funcion para agregar listener de borrado
-    const deleteBtn = document.querySelectorAll('.removeBtn')
-    deleteBtn.addEventListener('click', removeJoke(template));
+// Cargar chistes del localStorage
+const loadJokesStorage = () => {
+    const jokesLocalStorage = JSON.parse(localStorage.getItem('jokes')) || [];
+    return jokesLocalStorage;
 }
 
-console.log(localStorage)
+// Pintar el chiste en el documento
+const addNewJoke= (joke) => {
+    const li = document.createElement('li');
+    li.innerHTML = `
+        <h2>${joke}</h2>
+        <button class="removeBtn">Eliminar</button>
+    `;
+    jokeList.appendChild(li);
+    addDeleteListener(li, joke);
+}
 
-loadJokesPage()
-getJokeApi()
+//agregar evento a los botones de eliminar
+const addDeleteListener = (li, joke) => {
+    const removeBtn = li.querySelector('.removeBtn');
+    removeBtn.addEventListener('click', () => {
+        deleteJoke(joke, li);
+    });
+}
+
+// Eliminar un chiste del localStorage y del documento
+const deleteJoke = (joke, li) => {
+    let jokes = loadJokesStorage();
+    const index = jokes.indexOf(joke);
+    if (index !== -1) {     //JavaScript devuelve '-1' si no encuentra el elemento en el array
+        jokes.splice(index, 1);       // Eliminar el chiste del arreglo en la posición index
+        localStorage.setItem('jokes', JSON.stringify(jokes)); 
+        li.remove();
+    }
+}
+
+
+    loadJokesPage();
+    getJokeApi();
+
